29c1b5a182d5f231826e1c07886a1395440e7d06
ricardas.faturovas <ricardas.faturovas@gmail.com>
2018-02-20 15:59:27 +0200

    refactor(pizza-data): Refactor reducer to use entities

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,14 +1,14 @@
-import * as fromPizzas from '../actions/pizzas.actions';
-import { Pizza } from './../../models/pizza.model';
+import * as fromPizzas from "../actions/pizzas.actions";
+import { Pizza } from "./../../models/pizza.model";
 
 export interface PizzaState {
-  data: Pizza[]
-  loaded: boolean,
-  loading: boolean
+  entities: { [id: number]: Pizza };
+  loaded: boolean;
+  loading: boolean;
 }
 
 export const initialState: PizzaState = {
-  data: [],
+  entities: {},
   loaded: false,
   loading: false
 };
@@ -17,8 +17,7 @@ export function reducer(
   state = initialState,
   action: fromPizzas.PizzasActions
 ): PizzaState {
-
-  switch(action.type) {
+  switch (action.type) {
     case fromPizzas.LOAD_PIZZAS: {
       return {
         ...state,
@@ -27,12 +26,25 @@ export function reducer(
     }
 
     case fromPizzas.LOAD_PIZZAS_SUCCESS: {
-      const data = action.payload
+      const pizzas = action.payload;
+
+      const entities = pizzas.reduce(
+        (entities: { [id: number]: Pizza }, pizza: Pizza) => {
+          return {
+            ...entities,
+            [pizza.id]: pizza
+          }
+        },
+        {
+          ...state.entities
+        }
+      );
+
       return {
         ...state,
         loading: false,
         loaded: true,
-        data
+        entities
       };
     }
 
@@ -48,8 +60,6 @@ export function reducer(
   return state;
 }
 
+export const getPizzasEntities = (state: PizzaState) => state.entities;
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
-
-
