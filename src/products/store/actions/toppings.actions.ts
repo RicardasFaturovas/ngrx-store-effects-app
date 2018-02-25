874b6626bceb047a7ce0a50fdf4777c4d2deaaea
Ricardas Faturovas <rfaturovas@exadel.com>
2018-02-25 13:13:24 +0200

    feat(toppings): Add new toppings action

--- src/products/store/actions/toppings.actions.ts
+++ src/products/store/actions/toppings.actions.ts
@@ -6,6 +6,8 @@ export const LOAD_TOPPINGS = "[Products]: Load Toppings";
 export const LOAD_TOPPINGS_SUCCESS = "[Products]: Load Toppings Success";
 export const LOAD_TOPPINGS_FAIL = "[Products]: Load Toppings Fail";
 
+export const VISUALISE_TOPPINGS = '[Producs]: Visualise toppings'
+
 export class LoadToppings implements Action {
   readonly type = LOAD_TOPPINGS;
 }
@@ -20,8 +22,14 @@ export class LoadToppingsFail implements Action {
   constructor(public payload: any) {}
 }
 
+export class VisualiseToppings implements Action {
+  readonly type = VISUALISE_TOPPINGS;
+  constructor(public payload: number[]) {}
+}
+
 // action types
 export type ToppingsAction =
   | LoadToppings
   | LoadToppingsSuccess
-  | LoadToppingsFail;
+  | LoadToppingsFail
+  | VisualiseToppings;
